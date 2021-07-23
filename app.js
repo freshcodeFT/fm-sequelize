5f9dc927501fe86eddf926760eb0e71e1509eeab
Nikolay <freshitftm@gmail.com>
2021-07-23 12:15:58 +0300

    Add error handler

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const router = require('./router');
+const router = require('./routes');
+const errorHandler = require('./middlewares/error.handlers.mw');
 
 const app = express();
 app.use(express.json()); // data stream -> json -> js object -> req.body
 app.use('/api', router);
-
+app.use(errorHandler);
 
 module.exports = app;
