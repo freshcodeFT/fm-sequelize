031fda1c65e57d23da7234bf3e31904861e64ec1
Nikolay <freshitftm@gmail.com>
2021-07-27 10:30:25 +0300

    Add dotenv

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const { env } = require('process');
 
 module.exports = {
-  STATIC_PATH: path.resolve(__dirname, '../public'),
+  STATIC_PATH: env.STATIC_PATH || path.resolve(__dirname, '../public'),
   DB_CONFIG: path.resolve(__dirname, 'db.json'),
 };
